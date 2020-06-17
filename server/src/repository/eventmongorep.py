632bd6dfe088b152fcbe13d0be5f792af5bca07f
Riccardo <Riccardo Malavolti>
2020-06-17 16:57:45 +0200

    Add Player and Event repositories aganist MongoDB

---
+++ server/src/repository/eventmongorep.py
@@ -0,0 +1,16 @@
+class EventMongoRepository:
+    
+    database_address = "localhost"
+
+    def __init__(self):
+        self.client = "Opens a connection with mongo"
+
+    def add_event(self, event):
+        pass
+
+    def remove_event(self, event):
+        pass
+
+    def update_event(self, event):
+        pass
+
---
+++ server/src/repository/playermongorep.py
@@ -0,0 +1,18 @@
+class PlayerMongoRepository:
+    
+    database_address = "localhost"
+
+    def __init__(self):
+        self.client = "Opens a connection with mongo"
+    
+    def get_players(self):
+        return ['jon', 'mickey', 'brad']
+
+    def add_player(self, player):
+        pass
+
+    def remove_player(self, player):
+        pass
+
+    def update_player(self, player):
+        pass
